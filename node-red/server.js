115f58f3c8605d1be2a6ecc370c8b98cfffac80d
Ryan Mazali <ryanmazali17@gmail.com>
2025-10-25 14:03:35 -0300

    finalizing changes to host the bot

---
+++ node-red/server.js
@@ -0,0 +1,23 @@
+const http = require("http");
+const RED = require("node-red");
+
+// Cria o servidor HTTP
+const server = http.createServer();
+
+// Configurações do Node-RED
+const settings = {
+  httpAdminRoot: "/red",    // painel do editor
+  httpNodeRoot: "/",        // endpoints das flows
+  userDir: "/data",         // diretório de dados
+  functionGlobalContext: {}
+};
+
+// Inicializa o Node-RED
+RED.init(server, settings);
+
+// Escuta na porta fornecida pelo Render
+server.listen(process.env.PORT || 1880);
+
+// Inicia o Node-RED
+RED.start();
+
